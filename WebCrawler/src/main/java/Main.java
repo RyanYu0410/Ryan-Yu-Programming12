8ddfbe973b4c88a68e5a727badb2502e47412cb9
RyanYu0410 <99051928+RyanYu0410@users.noreply.github.com>
2022-05-31 17:13:33 -0700

    Update Main.java

--- WebCrawler/src/main/java/Main.java
+++ WebCrawler/src/main/java/Main.java
@@ -8,25 +8,25 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) {
-        // 利用Jsoup获得连接
+        // Using JSoup
         Connection connect = Jsoup.connect("https://www.nike.com/ca/w/lifestyle-shoes-13jrmzy7ok");
         try {
-            // 得到Document对象
+            // Get Document
             Document document = connect.get();
-            // 查找所有img标签`
+            // Find img tag
             Elements imgs = document.getElementsByTag("img");
-            System.out.println("共检测到下列图片URL：");
-            System.out.println("开始下载");
-            // 遍历img标签并获得src的属性
+            System.out.println("img URL：");
+            System.out.println("Start downloading");
+            // Find src in img tag
             for (Element element : imgs) {
-                //获取每个img标签URL "abs:"表示绝对路径
+                //URL "abs:"
                 String imgSrc = element.attr("abs:src");
-                // 打印URL
+                // Print URL
                 System.out.println(imgSrc);
-                //下载图片到本地
+                //Download img
                 DownloadImage.DownloadImages("d:/img", imgSrc);
             }
-            System.out.println("下载完成");
+            System.out.println("Finish downloading");
         } catch (IOException e) {
             e.printStackTrace();
         }
